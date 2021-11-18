d5e618231b34ed42951bd96fc728dbef989f0fe3
z9fr <z9fr@pm.me>
2021-11-18 11:24:36 +0530

    Added Logout option for users

--- src/main/java/com/example/xyzhotel/servletz/logout.java
+++ src/main/java/com/example/xyzhotel/servletz/logout.java
@@ -1,6 +1,22 @@
 package com.example.xyzhotel.servletz;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
+@WebServlet(name = "logout", value = "/logout")
 public class logout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.invalidate();
+        String errorMessage="You have Logged out Successfully!";
+        req.setAttribute("error", errorMessage);
+        req.getRequestDispatcher("/jsp/login.jsp").forward(req, resp);
+
+    }
 }
