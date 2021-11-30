dd98321e12bdbf7976c019af1764838f89422233
z9fr <z9fr@pm.me>
2021-11-30 18:58:42 +0530

    reviewing the payment

--- src/main/java/com/example/xyzhotel/servletz/payment/review_payment.java
+++ src/main/java/com/example/xyzhotel/servletz/payment/review_payment.java
@@ -1,7 +1,10 @@
 package com.example.xyzhotel.servletz.payment;
 
 import com.example.xyzhotel.dao.payment.PaymentService;
+import com.paypal.api.payments.PayerInfo;
 import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.ShippingAddress;
+import com.paypal.api.payments.Transaction;
 import com.paypal.base.exception.PayPalException;
 import com.paypal.base.rest.PayPalRESTException;
 
@@ -30,6 +33,18 @@ public class review_payment extends HttpServlet {
         {
             PaymentService paymentService = new PaymentService();
             Payment payment = paymentService.getPaymentDetails(paymentId);
+
+            // getting the payment details
+
+            PayerInfo payerInfo = payment.getPayer().getPayerInfo();
+            Transaction transaction = payment.getTransactions().get(0);
+
+            req.setAttribute("payer", payerInfo);
+            req.setAttribute("transaction", transaction);
+
+            String url = "/review.jsp?pay="+paymentId+"&payerid="+payerId;
+
+            req.getRequestDispatcher(url).forward(req, resp);
 
 
         } catch (PayPalRESTException e) {
