70181be5c71284d29b7dfbd2bf0b2ce55eea57ac
z9fr <z9fr@pm.me>
2021-11-30 22:37:23 +0530

    Added routes for /signup GET POST

--- src/main/java/com/example/xyzhotel/servletz/auth/register.java
+++ src/main/java/com/example/xyzhotel/servletz/auth/register.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "sign up" , value = "/signup")
 public class register extends HttpServlet {
@@ -18,6 +19,25 @@ public class register extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String uname = req.getParameter("username");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        PrintWriter io = resp.getWriter();
+        io.println("<html> <body>");
+        io.println("[+] Username = " + uname +"<br>");
+        io.println("[+] email = " + email);
+
+        // verify username
+
+        // make email with hash
+
+        // send email
+
+        // confirm email
+
+
 
     }
 }
