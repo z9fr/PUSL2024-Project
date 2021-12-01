c93393e8688573122545b48837aa3b62f41c68b6
z9fr <z9fr@pm.me>
2021-12-01 10:47:35 +0530

    update the user table with token

--- src/main/java/com/example/xyzhotel/dao/auth/ConfirmToken/UpdateUserVerification.java
+++ src/main/java/com/example/xyzhotel/dao/auth/ConfirmToken/UpdateUserVerification.java
@@ -1,4 +1,30 @@
 package com.example.xyzhotel.dao.auth.ConfirmToken;
 
+import com.example.xyzhotel.dao.dbconnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class UpdateUserVerification {
+    public boolean updateVerification(String uname, String token) throws SQLException {
+        Boolean isUpdated = false;
+
+        // UPDATE users set isVerified=false WHERE uname="z9fr" AND verificationid="f41f6bae-c5f4-4e92-a534-c4e49c7a704a";
+        Connection connection = dbconnection.getConnectionToDatabase();
+        String sql = "UPDATE users set isVerified=true WHERE uname=? AND verificationid=?";
+        java.sql.PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1 , uname);
+        statement.setString(2, token);
+
+        int set = statement.executeUpdate();
+
+        System.out.println("[+] info : User Added = " + set );
+
+        if(set == 1){
+            isUpdated = true;
+        }
+
+        return isUpdated;
+
+    }
 }
--- src/main/java/com/example/xyzhotel/servletz/auth/confirmEmail.java
+++ src/main/java/com/example/xyzhotel/servletz/auth/confirmEmail.java
@@ -1,5 +1,6 @@
 package com.example.xyzhotel.servletz.auth;
 
+import com.example.xyzhotel.dao.auth.ConfirmToken.UpdateUserVerification;
 import com.example.xyzhotel.dao.auth.ConfirmToken.getUserRequestedToken;
 import com.example.xyzhotel.dao.auth.ConfirmToken.validateToken;
 
@@ -28,6 +29,7 @@ public class confirmEmail extends HttpServlet {
 
         validateToken vt = new validateToken();
         getUserRequestedToken reqtoken = new getUserRequestedToken();
+        UpdateUserVerification updateUserVerification = new UpdateUserVerification();
 
         try {
             boolean isAlive = vt.isTokenAlive(tokenId, token);
@@ -41,11 +43,12 @@ public class confirmEmail extends HttpServlet {
 
                 // make user valid
 
+                boolean userUpdated = updateUserVerification.updateVerification(tokenOwner, tokenId);
 
+                if(userUpdated){
+                    // kill the token
 
-
-                // kill the token
-
+                }
             }
             else{
                 io.println("token invalid");
