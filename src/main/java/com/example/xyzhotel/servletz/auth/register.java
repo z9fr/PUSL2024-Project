cd50f248314778fc7ca35e9afcd3e5b9819f8d03
z9fr <z9fr@pm.me>
2021-12-01 09:20:02 +0530

    generating client secret for mail validation

--- src/main/java/com/example/xyzhotel/servletz/auth/register.java
+++ src/main/java/com/example/xyzhotel/servletz/auth/register.java
@@ -1,6 +1,8 @@
 package com.example.xyzhotel.servletz.auth;
 
 import com.example.xyzhotel.dao.auth.createUser.checkUserExist;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
 @WebServlet(name = "sign up" , value = "/signup")
 public class register extends HttpServlet {
@@ -46,7 +50,13 @@ public class register extends HttpServlet {
             }
 
             else {
+
+                String verificationid = getUniqueKey();
+                String token = getUniqueKey();
+
                 writer.println("<code> valid user </code>");
+                writer.println("<code> id = "+verificationid+ "</code> <br>");
+                writer.println("<code> token = "+token+ "</code> <br>");
             }
 
             // verify username
@@ -75,4 +85,29 @@ public class register extends HttpServlet {
         }
         return result;
     }
+
+    public String getUniqueKey() {
+        String uniqueKey = String.valueOf(UUID.randomUUID());
+        return uniqueKey;
+    }
+
+    public String getToken(String email , String uname) throws NoSuchAlgorithmException {
+        Dotenv dotenv = null;
+        dotenv = Dotenv.configure().load();
+        String secret =dotenv.get("SIGN_UP_SECRET");
+
+        // generating a confirmation using email username and sign it with a secret
+        String tohashString = email+uname+secret;
+
+        // hash this string sha256
+
+        String sha256hex = DigestUtils.shaHex(tohashString);
+
+        System.out.println("[*] Debug : Secret Generated for User "+uname+ "\nToken => "+sha256hex);
+
+        return sha256hex;
+    }
+
+
+
 }
