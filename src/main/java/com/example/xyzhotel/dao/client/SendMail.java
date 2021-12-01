bd5638ea85b2b34807ade574bfea58f2f6dbb8cf
z9fr <z9fr@pm.me>
2021-12-01 06:44:14 +0530

    email config is complete

---
+++ src/main/java/com/example/xyzhotel/dao/client/SendMail.java
@@ -0,0 +1,65 @@
+package com.example.xyzhotel.dao.client;
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+    public static void main(String[] args) {
+
+        String sendTo = "z9fr@pm.me";
+        String mailFrom = "xyzhotel@mail.com";
+        String mailHost = "smtp.mail.com";
+
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", mailHost);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // get session obj and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("xyzhotel@mail.com", "");
+            }
+        });
+
+        session.setDebug(true); // debug smtp
+
+        try {
+            // create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(mailFrom));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendTo));
+
+            // set subject and content
+            message.setSubject("This is the Subject Line!");
+            message.setContent(
+                    "<h1>This is actual message embedded in HTML tags</h1>",
+                    "text/html");
+
+            System.out.println("[*] Debug : Sending the mail ");
+
+            // send the mail
+            Transport.send(message);
+            System.out.println("[+] Email Send Successfully");
+        }
+        catch (MessagingException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/example/xyzhotel/dao/client/test.java
@@ -0,0 +1,7 @@
+package com.example.xyzhotel.dao.client;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(String.format("The current shell is: %s.", dotenv.get("OHHELOFRIEND")));
+    }
+}
